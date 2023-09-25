dfbe98ea8e4d34130077f6a1906a0bbdf25b4f65
Tazin Sultana <tazimsultana457@gmail.com>
2023-09-25 20:21:53 +0600

    role management

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@gmail.com',
+                'password' => bcrypt('12345678'),
+                'type' => 1,
+
+
+            ],
+            [
+                'name' => 'User',
+                'email' => 'user@gmail.com',
+                'password' => bcrypt('12345678'),
+                'type' => 0,
+
+
+
+            ],
+
+
+
+        ];
+        foreach ($users as $user)
+            User::create($user);
+    }
+}
